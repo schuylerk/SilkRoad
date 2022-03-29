2836d5450dd60ea6afba52876fd0a1d0f2e6d572
student <student@studentdeiMac.local>
2022-03-29 20:35:49 +0800

    vr

---
+++ SilkRoad/SilkRoad/Classes/Business/VR/Controller/IntroductionCultureRelicViewController.swift
@@ -0,0 +1,18 @@
+//
+//  IntroductionCultureRelicViewController.swift
+//  SilkRoad
+//
+//  Created by student on 2022/3/29.
+//
+
+import UIKit
+
+class IntroductionCultureRelicViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
